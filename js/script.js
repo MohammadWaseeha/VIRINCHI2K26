6b7ac200b7636db75b4931e327947023ad36914d
MohammadWaseeha <waseehamohammad@gmail.com>
2026-02-23 20:15:28 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,422 @@
+const technicalEvents = {
+    cse: [
+        {
+            name: "Poster Presentation",
+            gif: "poster.gif",
+            description: "Candidates have to present the given theme in the form of poster (Chart) using paints/ crayons/sketches/paper crafts etc... <br><b>Theme: 6G & Hyperconnected Societies</b>.",
+            time: "16<sup>th</sup> March 2026, 03:00 PM - 04:00 PM",
+            venue: "MV-306 (Drawing Hall)",
+            participation: "Min. 2, Max. 4",
+            themes: [
+                "6G & Hyperconnected Societies"
+            ]
+        },
+        {
+            name: "Bug Blasters",
+            gif: "bugblasters.gif",
+            description: "Identify and fix errors in the given code within the time limit. Put your debugging skills, logical thinking, and attention to detail to the ultimate test.",
+            time: "16<sup>th</sup> March 2026, 02:00 PM - 03:00 PM",
+            venue: "MV-400, MV-401 & Computer Center 3",
+            participation: "Individual"
+        },
+        {
+            name: "Snake & Ladders",
+            gif: "s&l.gif",
+            description: "Roll the dice and face your fate — land on a ladder to double your points, hit a snake and lose points with a fun punishment, or answer a course-based technical question correctly to earn points and move ahead.",
+            time: "16<sup>th</sup> March 2026, 04:00 PM - 05:00 PM",
+            venue: "CV-209",
+            participation: "Individual"
+        },
+        
+        {
+            name: "JAM",
+            gif: "jam.gif",
+            description: "Speak non-stop for one minute on a given topic without hesitation, repetition, or deviation. Test your presence of mind, clarity of thought, and spontaneous speaking skills under pressure.",
+            time: "16<sup>th</sup> March 2026, 12:00 PM - 01:00 PM",
+            venue: "MV-207, MV-208",
+            participation: "Individual"
+        },
+        {
+            name: "Technical Charades",
+            gif: "charades.gif",
+            description: "A technical concept will be given to one team member by the organizers, who must enact it without words. The rest of the team gets 1 minute to identify the concept correctly.",
+            time: "16<sup>th</sup> March 2026, 11:00 AM - 12:00 PM",
+            venue: "MV-200, MV-201",
+            participation: "Min. 4 Members"
+        }
+    ],
+
+    ece: [
+        {
+            name: "Poster Presentation",
+            gif: "poster1.gif",
+            description: "A platform for students to present innovative ideas on emerging technologies through well-structured and research-oriented posters.",
+            time: "16<sup>th</sup> March 2026, 11:00 AM - 12:00 PM",
+            venue: "MV-306 (Drawing Hall)",
+            participation: "2 Members",
+            themes: [
+                "New Innovation Ideas",
+                "Trending Technologies(like AI, VLSI, Embedded Systems etc...)"
+            ]
+        },
+        {
+            name: "Brainwave Challenge",
+            gif: "brainwave.gif",
+            description: "A dynamic technical quiz designed to assess knowledge, analytical thinking, and quick decisionmaking skills.",
+            time: "16<sup>th</sup> March 2026, 03:00 PM - 04:00 PM",
+            venue: "CV-310",
+            participation: "Individual"
+        },
+        {
+            name: "Surge & Slide",
+            gif: "s&s.gif",
+            description: "An interactive technical game inspired by Snakes and Ladders, where progress depends on answering subject-based questions correctly.",
+            time: "16<sup>th</sup> March 2026, 01:00 PM - 02:00 PM",
+            venue: "MV-104",
+            participation: "2 Members"
+        },
+        {
+            name: "Treasure Hunt",
+            gif: "treasurehunt.gif",
+            description: "A team-based problem-solving challenge involving technical clues, logical reasoning, and strategic thinking.",
+            time: "16<sup>th</sup> March 2026, 02:00 PM - 03:00 PM",
+            venue: "MV-104, MV-105 & MV-106",
+            participation: "Individual"
+        },
+        {
+            name: "Project Expo",
+            gif: "projectexpo.gif",
+            description: "An exhibition platform for students to showcase innovative projects and working models demonstrating practical application and technical excellence.",
+            time: "16<sup>th</sup> March 2026, 12:00 PM - 01:00 PM",
+            venue: "MV-105",
+            participation: "Team of 3"
+        }
+    ],
+
+    eee: [
+        {
+            name: "Poster Presentation",
+            gif: "poster.gif",
+            description: "A visual presentation event where participants showcase research or project ideas using concise text, charts, and images, evaluated on clarity, content, and presentation.",
+            time: "16<sup>th</sup> March 2026, 02:00 PM - 03:00 PM",
+            venue: "MV-103",
+            participation: "2 Members"
+        },
+        {
+            name: "Rush to Seat",
+            gif: "rushtoseat.gif",
+            description: "A multi-round competition beginning with a written test, followed by technical musical chair rounds and a final quiz, with overall performance determining the Winner and Runner-up.",
+            time: "16<sup>th</sup> March 2026, 03:00 PM - 04:00 PM",
+            venue: "MV-103",
+            participation: "Individual"
+        },
+        {
+            name: "Shock & Act",
+            gif: "s&a.gif",
+            description: "A two-member team event where one participant enacts an electrical term silently while the other guesses within one minute, earning points for each correct answer.",
+            time: "16<sup>th</sup> March 2026, 12:00 PM - 01:00 PM",
+            venue: "MV-101",
+            participation: "2 Members"
+        },
+        {
+            name: "Path to Power",
+            gif: "pathtopower.gif",
+            description: "An exciting technical challenge where participants follow designated hand and foot paths while accurately connecting a circuit within the given time, testing balance, speed, and precision.",
+            time: "16<sup>th</sup> March 2026, 04:00 PM - 05:00 PM",
+            venue: "MV-102",
+            participation: "Individual"
+        },
+        {
+            name: "Balloon Bonanza",
+            gif: "balloonbonanza.gif",
+            description: "A two-round competition where participants first answer 25 questions to qualify, followed by a rapid round of bursting balloons, answering questions, and forming words—fastest and most accurate wins.",
+            time: "16<sup>th</sup> March 2026, 10:30 AM - 12:00 PM",
+            venue: "CV-101",
+            participation: "Team of 3"
+        }
+    ],
+
+    me: [
+        {
+            name: "Poster Presentation",
+            gif: "poster1.gif",
+            description: "A poster presentation is a visual method of presenting research or project ideas using images,charts, and brief text.",
+            time: "16<sup>th</sup> March 2026, 02:00 PM - 02:45 PM",
+            venue: "CV-204",
+            participation: "2 Members",
+            themes: [
+                "Sustainable Energy Systems",
+                "Robotics & Automation",
+                "AI in Mechanical Systems",
+                "Green Mobility",
+                "Innovations in Materials",
+                "Industrial IoT",
+                "Quantum Technology"
+            ]
+        },
+        {
+            name: "Quest of Secrets",
+            gif: "questofsecrets.gif",
+            description: "Quest of Secrets is an adventurous and thrilling activity where participants solve clues and uncover hidden mysteries.",
+            time: "16<sup>th</sup> March 2026, 04:00 PM - 05:00 PM",
+            venue: "CV-105",
+            participation: "Individual"
+        },
+        {
+            name: "Brain Battle",
+            gif: "brainbattle.gif",
+            description: "Brain Battle is an exciting intellectual competition where participants test their knowledge, logic, and quick thinking skills.",
+            time: "16<sup>th</sup> March 2026, 10:00 AM - 12:00 PM",
+            venue: "MV-309",
+            participation: "2 Members"
+        },
+        {
+            name: "Grind your Mind",
+            gif: "grindmind.gif",
+            description: "Grind Your Mind is a challenging intellectual event that pushes participants to think deeply and solve complex problems.",
+            time: "16<sup>th</sup> March 2026, 12:00 PM - 01:00 PM",
+            venue: "CV-310",
+            participation: "Individual"
+        },
+        {
+            name: "Drafting Marathon",
+            gif: "draftingmarathon.gif",
+            description: "Drafting Marathon is a competitive event where participants showcase their technical drawing and design skills within a limited time.",
+            time: "16<sup>th</sup> March 2026, 02:45 PM - 04:00 PM",
+            venue: "MV-306 (Drawing Hall)",
+            participation: "Individual"
+        }
+    ],
+};
+
+
+
+const departmentWatermarks = {
+    cse: "CELESTRA",
+    ece: "SADARWA",
+    eee: "VIDYUTSAV",
+    me: "MECHARENA"
+};
+
+
+
+const culturalEvents = [
+    {
+        name: "Miss Virinchi 2K26",
+        gif: "missvirinchi.gif",
+        description: "A personality and talent showcase celebrating confidence, elegance, and individuality, recognizing one Winner and one Runner-up.",
+        time: "17<sup>th</sup> March 2026, 11:00 AM - 01:00 PM",
+        venue: "Indira Priyadarshini Auditorium",
+        participation: "Individual"
+    },
+    {
+        name: "Natyakala",
+        gif: "natyakala.gif",
+        description: "Step onto the stage and bring your passion to life by performing in the dance style of your choice—Classical, Western, or Folk. Showcase your grace, energy, and expressions as you captivate the audience with your performance.",
+        time: "18<sup>th</sup> March 2026, 09:00 AM - 11:00 AM",
+        venue: "Indira Priyadarshini Auditorium",
+        participation: "Solo, Duet, or Group (5 members)"
+    },
+    {
+        name: "Swaranjali",
+        gif: "swaranjali.gif",
+        description: "A musical event with Solo, Duet, and Group (4 members) categories, judged on pitch, rhythm, expression, coordination, and overall performance.",
+        time: "17<sup>th</sup> March 2026, 09:00 AM - 11:00 AM",
+        venue: "Indira Priyadarshini Auditorium",
+        participation: "Solo, Duet, or Group (4 members)"
+    },
+    {
+        name: "HastaAlankriti",
+        gif: "hastaalankriti.gif",
+        description: "A hand-art and craft event highlighting creativity and craftsmanship, evaluated on design, precision, uniqueness, and overall aesthetic appeal.",
+        time: "17<sup>th</sup> March 2026, 11:00 AM - 12:00 PM",
+        venue: "KL-201",
+        participation: "Individual"
+    },
+    {
+        name: "Chitravalli",
+        gif: "chitravalli.gif",
+        description: "Chitravalli is a traditional Rangoli competition where participants create beautiful floor designs using colors and patterns.",
+        time: "17<sup>th</sup> March 2026, 02:00 PM - 03:00 PM",
+        venue: "Outdoors of Engineering Block",
+        participation: "Individual"
+    },
+    {
+        name: "Waste to Wonder",
+        gif: "wastetowonder.gif",
+        description: "A creative competition transforming waste materials into innovative and useful creations, judged on originality, sustainability, and presentation.",
+        time: "18<sup>th</sup> March 2026, 11:00 AM - 12:00 PM",
+        venue: "Outdoors of Engineering Block",
+        participation:"Individual"
+    },
+    {
+      name:"Ethnic Aura", 
+      gif:"ethnicaura.gif", 
+      description:"A cultural event celebrating traditional attire, evaluated on authenticity, confidence, creativity, and overall impact, with one Winner and one Runner-up.", 
+      time:"18<sup>th</sup> March 2026, 11:00 AM - 01:00 PM", 
+      venue:"Indira Priyadarshini Auditorium", 
+      participation:"Individual"
+    },
+    {
+      name:"Riddles & Proverbs", 
+      gif:"riddles&proverbs.gif", 
+      description:"A team event for four members testing wit and cultural knowledge through riddles and proverbs, awarding a Group Winner and Group Runner-up.", 
+      time:"17<sup>th</sup> March 2026, 09:00 AM - 11:00 AM", 
+      venue:"Drithi Hall, KL Rao Block", 
+      participation:"Team of 4"
+    },
+    {
+      name:"Grace the Scale", 
+      gif:"gracethescale.gif", 
+      description:"Grace the Scale is a drawing competition where participants express their creativity through sketches and artwork.", 
+      time:"17<sup>th</sup> March 2026, 12:00 PM - 01:00 PM", 
+      venue:"MV-306 (Drawing Hall)", 
+      participation:"Individual"
+    },
+    {
+      name:"Freeze the Frame", 
+      gif:"freezetheframe.gif", 
+      description:"A theme will be given on the spot, and participants must capture a photograph that best represents it. The most creative and theme-appropriate shot will be declared the winner.", 
+      time:"18<sup>th</sup> March 2026, 10:00 AM - 11:00 AM", 
+      venue:"KL-201", 
+      participation:"Individual"
+    },
+    {
+      name:"Soap Chiseling", 
+      gif:"foamtoform.gif", 
+      description:"Soap Chiseling is a creative soap art competition where participants carve and shape beautiful designs from soap.", 
+      time:"18<sup>th</sup> March 2026, 12:00 PM - 01:00 PM", 
+      venue:"CV-101",  
+       participation :"Individual"
+   },
+   {
+     name :"Path Finders" ,  
+     gif :"pathfinders.gif" ,  
+     description :"Kickstart the hunt with the first clue and unravel a series of consecutive hints that guide you toward the hidden treasure. Analyze, navigate, and outpace others to reach the final destination." ,  
+     time :"17<sup>th</sup> March 2026, 03:00 PM - 05:00 PM",  
+     venue :"Outdoors of Engineering Block" ,  
+     participation :"Team of 4"  
+   }
+];
+
+function createCard(event, dept = null){
+
+    let watermarkText = "";
+
+    if(dept && departmentWatermarks[dept]){
+        watermarkText = `<span class="back-watermark">${departmentWatermarks[dept]}</span>`;
+    }
+
+    return `
+    <div class="card" onclick="this.classList.toggle('flip')">
+        <div class="card-inner">
+
+            <div class="card-front">
+                <h3 class="event-title">${event.name}</h3>
+                <div class="gif-container">
+                    <img src="assets/gifs/${event.gif}" alt="${event.name}">
+                </div>
+                <p class="click-hint">Click for more details</p>
+            </div>
+
+            <div class="card-back">
+                
+                <h4 class="event-back-title">${event.name}</h4>
+
+                <p class="event-description">
+                    ${event.description}
+                </p>
+
+                <div class="event-meta">
+                    <div class="meta-item">
+                        <i class="fa-regular fa-calendar"></i>
+                        <span>${event.time}</span>
+                    </div>
+
+                    <div class="meta-item">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>${event.venue}</span>
+                    </div>
+
+                    <div class="meta-item">
+                        <i class="fa-solid fa-users"></i>
+                        <span>${event.participation}</span>
+                    </div>
+                </div>
+                ${event.themes ? `
+                    <div class="themes-section">
+                        <div class="themes-toggle" onclick="toggleThemes(event, this)">
+                            <i class="fa-solid fa-layer-group"></i>
+                            <span>View Themes</span>
+                        </div>
+
+                        <ul class="themes-list">
+                            ${event.themes.map(theme => `<li>${theme}</li>`).join("")}
+                        </ul>
+                    </div>
+                ` : ""}
+            </div>
+
+
+
+        </div>
+    </div>
+    `;
+}
+
+// function toggleThemes(element){
+//     const list = element.nextElementSibling;
+//     list.classList.toggle("show");
+// }
+
+function toggleThemes(e, element){
+    e.stopPropagation();  // 🔥 prevents card flip
+
+    const list = element.nextElementSibling;
+    list.classList.toggle("show");
+}
+
+
+function loadDepartment(dept){
+    const container = document.getElementById("events-container");
+    container.innerHTML = "";
+
+    technicalEvents[dept].forEach(event=>{
+        container.innerHTML += createCard(event, dept);
+    });
+}
+
+function loadCultural(){
+    const container=document.getElementById("cultural-container");
+    culturalEvents.forEach(ev=>{
+        container.innerHTML+=createCard(ev);
+    });
+}
+
+/* TAB CLICK LOGIC */
+document.querySelectorAll(".dept-tabs button").forEach(button=>{
+    button.addEventListener("click", function(){
+
+        document.querySelectorAll(".dept-tabs button")
+            .forEach(btn=>btn.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const dept = this.getAttribute("data-dept");
+        loadDepartment(dept);
+    });
+});
+
+/* DEFAULT LOAD → CSE */
+window.addEventListener("DOMContentLoaded", ()=>{
+    loadDepartment("cse");
+    loadCultural();
+});
+
+document.getElementById("exploreBtn").addEventListener("click", function(){
+
+    const options = document.getElementById("eventTypeOptions");
+
+    options.classList.toggle("active");
+
+});
